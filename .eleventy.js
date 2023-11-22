913dab601d87238bf5abb7a7fc98fccd7b9f2875
SB <shanisebarona@gmail.com>
2023-11-21 23:45:21 -0500

    adds yaml data 📊

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,8 @@
+const yaml = require("js-yaml");
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addLayoutAlias('base', 'layouts/base.njk');
+  eleventyConfig.addDataExtension("yaml", contents => yaml.load(contents));
     return {
       dir: {
         input: "src",
